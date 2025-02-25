1c60ed50207154c20214fb6c0cc3b60f3b9c1412
Italo Pereira <italopereiradeoliveira@hotmail.com>
2025-02-24 21:32:46 -0300

    added tryCatch

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+    int result = n1 / n2;
+    Console.WriteLine(result);
+} catch (DivideByZeroException)
+{
+    Console.WriteLine("Division by 0 isn't allowed!");
+} catch (FormatException)
+{
+    Console.WriteLine("Only numbers allowed!");
+}
